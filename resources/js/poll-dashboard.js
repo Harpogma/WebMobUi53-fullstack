4fea7e95deb0c69df96350f835d3acd959d52e64
Harpogma <gd.temp@pm.me>
2026-04-27 11:04:52 +0200

    add a button to access the dashboard, and integrated Quasar framework for the frontend

--- resources/js/poll-dashboard.js
+++ resources/js/poll-dashboard.js
@@ -1,8 +1,25 @@
 import './bootstrap';
-import { createApp } from 'vue';
+
 import App from './AppPollDashboard.vue';
+
+import { createApp } from "vue";
+import { Quasar } from "quasar";
+
+import "@quasar/extras/material-icons/material-icons.css";
+import "quasar/dist/quasar.css";
+
 
 const el = document.getElementById('app');
 const props = JSON.parse(el.dataset.props ?? '{}');
+const myApp = createApp(App, props);
 
-createApp(App, props).mount(el);
+myApp.use(Quasar, {
+    config: {
+        brand: {
+            // Définissez ici vos couleurs de thème personnalisées
+        },
+    },
+    plugins: {},
+});
+
+myApp.mount(el);
--- vite.config.js
+++ vite.config.js
@@ -2,27 +2,36 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
 export default defineConfig({
     plugins: [
-        vue(),
+        vue({
+            template: { transformAssetUrls },
+        }),
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/poll-dashboard.js',
-                'resources/js/poll-dashboard-integrated.js',
+                "resources/css/app.css",
+                "resources/js/poll-dashboard.js",
+                "resources/js/poll-dashboard-integrated.js",
             ],
             refresh: true,
         }),
         tailwindcss(),
+        quasar(),
     ],
     server: {
         host: true,
         hmr: {
-            host: 'localhost'
+            host: "localhost",
         },
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            ignored: ["**/storage/framework/views/**"],
+        },
+    },
+    resolve: {
+        alias: {
+            "@": "/src",
         },
     },
 });
